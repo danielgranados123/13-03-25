097c32fe503ef1a28d6f30ae7079c6081c2199c1
Daniel Granados <20200008@ricaldone.edu.sv>
2025-03-13 11:20:38 -0600

    api 3

--- js/api3.js
+++ js/api3.js
@@ -1,26 +1,23 @@
-const getApi2Characters = async () => {
+const getApi3Characters = async () => {
     try {
-const response = await fetch('https://rickandmortyapi.com/api/character');
-        if (!response.ok) throw new Error('Error al obtener datos');
- 
-        const data = await response.json();
- 
-        const cardsParent = document.getElementById('cards');
-        data.data.forEach(api2 => {
-            const card = document.createElement('div');
-            card.innerHTML = `
-                <div class="dogs">
-                    <div class="dog">
-                        <h2>${api2.results.name}</h2>
-                        <p>${api2.results.status}</p>
-                    </div>
-                </div>
-            `;
-            cardsParent.appendChild(card);
-        });
-    } catch (error) {
-        console.error("Error:", error.message);
-    }
-};
+  const response = await fetch('https://api.coingecko.com/api/v3/coins/bitcoin');
+      if (!response.ok) throw new Error('Error al obtener datos');
+      const data = await response.json();
+      const cardsParent = document.getElementById('cards');
 
-getApi2Characters();
+      const card = document.createElement('div');
+      card.innerHTML = `
+        <div class="dogs">
+          <div class="dog">
+            <h2>${data.name}</h2>
+            <p>${data.description.en}</p>
+          </div>
+        </div>
+      `;
+      cardsParent.appendChild(card);
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  };
+   
+  getApi3Characters();
